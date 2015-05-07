fcd9eb0e690404385fb60b88124f8bfdec983e15
Tan Theng Lee <thengleetan@gmail.com>
2015-05-07 22:21:43 +0800

    Serve static files

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-	res.status('OK').send('Hello world!');
-});
+app.use(express.static('public'));
+
 
 app.listen(8000, function(){
 	console.log('Listening on port 8000...');
